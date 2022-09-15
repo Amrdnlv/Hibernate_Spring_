6966e5b74f254a9b8d961e2e1062043fd4bad18f
Amrdnlv <onne.onne@mail.ru>
2022-09-15 22:57:49 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernate_spring/HibernateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateSpringApplication.class, args);
+    }
+
+}
