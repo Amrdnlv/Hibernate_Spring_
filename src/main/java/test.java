19464163c12de6a8bc8d13192f3fe3b0b6366837
Amrdnlv <onne.onne@mail.ru>
2022-09-16 14:23:45 +0300

    create main classs with error

---
+++ src/main/java/test.java
@@ -0,0 +1,26 @@
+
+import Entity.Emploee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class test {
+    public static void main(String[] args)
+    {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Emploee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            Emploee emp = new Emploee("Roman", "Danilov", "ID", "50000");
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+        }finally {
+            factory.close();
+        }
+
+
+    }
+}
