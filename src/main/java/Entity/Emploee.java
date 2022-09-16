328cc67b3d927724ce306fe061fa81bcc75ccad5
Amrdnlv <onne.onne@mail.ru>
2022-09-16 14:22:49 +0300

    add anotation for class

--- src/main/java/Entity/Emploee.java
+++ src/main/java/Entity/Emploee.java
@@ -1,9 +1,6 @@
 package Entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "employee")
