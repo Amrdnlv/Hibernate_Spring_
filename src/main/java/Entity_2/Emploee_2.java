bc7cb2b78ff27e26f6619a1b88cda0a020b719af
Amrdnlv <onne.onne@mail.ru>
2022-09-19 01:35:42 +0300

    Add new class, and make one-to-one

---
+++ src/main/java/Entity_2/Emploee_2.java
@@ -0,0 +1,84 @@
+package Entity_2;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee_2")
+public class Emploee_2 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "salary")
+    private Integer salary;
+    @OneToOne(cascade = CascadeType.ALL)//указываем принцип связи, и тип каскада тип удаление всех заввисимых полей
+    @JoinColumn(name = "details_id") //на какой столбец ссылается,
+    // по какому полю идет соединение
+    private Details empDetail;
+
+    public Emploee_2(){}
+
+    public Emploee_2(String name, String surname, String department, Integer salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Emploee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/Entity_2/test_2.java
+++ src/main/java/Entity_2/test_2.java
@@ -1,42 +1,43 @@
-package Entity;
+package Entity_2;
 
-import Entity.Emploee;
+import Entity_2.Emploee_2;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
-public class test {
+public class test_2 {
     public static void main(String[] args)
     {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Emploee.class)
+                .addAnnotatedClass(Emploee_2.class)
+                .addAnnotatedClass(Details.class) //добавляем зависимый класс
                 .buildSessionFactory();
         Session session = null;
         try {
            session = factory.getCurrentSession();
-            Emploee emp = new Emploee("Roman", "Danilov", "ID", 50000);
+            Emploee_2 emp = new Emploee_2("Roman", "Danilov", "ID", 50000);
             session.beginTransaction();
             session.save(emp);
             int getid = emp.getId(); // получение созданного работника
-            Emploee employ = session.get(Emploee.class, getid);
+            Emploee_2 employ = session.get(Emploee_2.class, getid);
             //создание Select по айди
             System.out.print(employ);
 
 
           //HQL запрос Получение всех работников
-            List <Emploee> emploees= session.createQuery("FROM Emploee").getResultList();
-            for (Emploee e: emploees)
+            List <Emploee_2> emploees= session.createQuery("FROM Emploee_2").getResultList();
+            for (Emploee_2 e: emploees)
                 System.out.print(e);
             //поиск работников с именем Roman
-            List <Emploee> emploeess= session.createQuery("FROM Emploee WHERE name = 'Roman'").getResultList();
-            for (Emploee e: emploeess)
+            List <Emploee_2> emploeess= session.createQuery("FROM Emploee_2 WHERE name = 'Roman'").getResultList();
+            for (Emploee_2 e: emploeess)
                 System.out.print(e);
 
             //Обновление записи работника, обновляем зарплату
-            Emploee employeUp = session.get(Emploee.class, 4);
+            Emploee_2 employeUp = session.get(Emploee_2.class, 4);
             //employeUp.setSalary(1500);
 
             //Обновляем данные запролаты у сотрудников по имени Роман
